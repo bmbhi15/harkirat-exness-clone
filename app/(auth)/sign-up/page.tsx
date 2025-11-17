a76ae042c416491f84fbee58b5092c794fb1ba50
bmbhi15 <admin@Admins-MacBook-Pro.local>
2025-11-17 20:13:41 +0530

    Implemented basic sign up and log in page without form field

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,5 +1,22 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function SignUp() {
-  return <div></div>;
+  return (
+    <div className="space-y-4 mx-40">
+      <p className="form-title">Sign Up & Personalize</p>
+      <div className="form w-full">
+        <Button className="yellow-btn w-full">
+          Start Your Investing Journey
+        </Button>
+      </div>
+      <div className="flex flex-row w-full space-x-2 items-center justify-center">
+        <p>Already have an account ?</p>
+        <Link href="./log-in">
+          <p className="text-white">Log In</p>
+        </Link>
+      </div>
+    </div>
+  );
 }
